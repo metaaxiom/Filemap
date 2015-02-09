2543d818d55a93ec3ff30615a98ae9800a9356a6
Julian Derwisz <JDerwisz@users.noreply.github.com>
2015-02-09 12:23:45 -0500

    Better Structure

--- index.php
+++ index.php
@@ -1,31 +1,23 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<!--Fonts-->
 		<link href='http://fonts.googleapis.com/css?family=Roboto:900,900italic,500,400italic,100,700italic,300,700,500italic,100italic,300italic,400' rel='stylesheet' type='text/css'>
-		<link href="css-sprite/sprite-av-black.css" rel="stylesheet">
+		<!--Font Awesome-->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<!--Styling-->
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<div class="repo">
-			<div class="repo-folder">
-				<div class="repo-folder-name">Folder 1</div>
-				<div class="repo-file">
-					<div class="repo-file-name">File 1</div>
-				</div>
+			<div class="repo-folder">Folder 1
+				<div class="repo-file">File 1</div>
 			</div>
-			<div class="repo-folder">
-				<div class="repo-folder-name">Folder 2</div>
-				<div class="repo-file">
-					<div class="repo-file-name">File 1</div>
-				</div>
-				<div class="repo-file">
-					<div class="repo-file-name">File 2</div>
-				</div>
-				<div class="repo-folder">
-					<div class="repo-folder-name">Inside Folder</div>
-					<div class="repo-file">
-						<div class="repo-file-name">File 1</div>
-					</div>
+			<div class="repo-folder">Folder 2
+				<div class="repo-file">File 1</div>
+				<div class="repo-file">File 2</div>
+				<div class="repo-folder">Inside Folder
+					<div class="repo-file">File 1</div>
 				</div>
 			</div>
 		</div>
