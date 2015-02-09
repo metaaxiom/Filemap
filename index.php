9631319b23ae36e722550508a32d7b87c9c95068
Julian Derwisz <julianderwisz@gmail.com>
2015-02-08 23:56:30 -0500

    Initialized directory

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:900,900italic,500,400italic,100,700italic,300,700,500italic,100italic,300italic,400' rel='stylesheet' type='text/css'>
+		<link href="css-sprite/sprite-av-black.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="repo">
+			<div class="repo-folder">
+				<div class="repo-folder-name">Folder 1</div>
+				<div class="repo-file">
+					<div class="repo-file-name">File 1</div>
+				</div>
+			</div>
+			<div class="repo-folder">
+				<div class="repo-folder-name">Folder 2</div>
+				<div class="repo-file">
+					<div class="repo-file-name">File 1</div>
+				</div>
+				<div class="repo-file">
+					<div class="repo-file-name">File 2</div>
+				</div>
+				<div class="repo-folder">
+					<div class="repo-folder-name">Inside Folder</div>
+					<div class="repo-file">
+						<div class="repo-file-name">File 1</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
