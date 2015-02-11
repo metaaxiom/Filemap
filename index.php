413ca8ff1e35d912a08ef6a78791ab4c964bcc9f
Julian Derwisz <julianderwisz@gmail.com>
2015-02-10 23:45:36 -0500

    Under development

--- index.php
+++ index.php
@@ -7,19 +7,49 @@
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<!--Styling-->
 		<link rel="stylesheet" type="text/css" href="style.css">
+
+		<link rel="import" href="components/paper-dropdown/paper-dropdown.html">
 	</head>
 	<body>
-		<div class="repo">
-			<div class="repo-folder">Folder 1
-				<div class="repo-file">File 1</div>
-			</div>
-			<div class="repo-folder">Folder 2
-				<div class="repo-file">File 1</div>
-				<div class="repo-file">File 2</div>
-				<div class="repo-folder">Inside Folder
-					<div class="repo-file">File 1</div>
-				</div>
-			</div>
+		<div class="filemap">
+			<div class="filemap-description">This is a directory description</div>
+			<ul class="folder-wrap">
+				<li class="folder-name"><i class="fa fa-folder-open-o"></i> Folder 1</li>
+				<li class="file"><i class="fa fa-file-code-o"></i> File 1</li>
+				<li class="file"><i class="fa fa-file-code-o"></i> File 2</li>
+				<li class="file"><i class="fa fa-file-code-o"></i> File 3</li>
+				<ul class="folder-wrap">
+					<li class="folder-name"><i class="fa fa-folder-open-o"></i> Folder 2</li>
+					<li class="file"><i class="fa fa-file-code-o"></i> File 1</li>
+					<li class="file"><i class="fa fa-file-code-o"></i> File 2</li>
+				</ul>
+			</ul>
 		</div>
+
+
+		<div class="accordion vertical">
+		  <section id="vertabout">
+		      <h2><a href="#vertabout">Folder One</a></h2>
+		      <p><ul><li>file 1</li><li>file 2</li></ul></p>
+		  </section>
+		  <section id="vertservices">
+		      <h2><a href="#vertservices">Folder Two</a></h2>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id lobortis massa. Nunc viverra velit leo, sit amet elementum mi. Fusce posuere nunc a mi tempus malesuada. Curabitur facilisis rhoncus eros eget placerat. Aliquam semper mauris sit amet justo tempor nec lacinia magna molestie. Etiam placerat congue dolor vitae adipiscing. Aliquam ac erat lorem, ut iaculis justo. Etiam mattis dignissim gravida. Aliquam nec justo ante, non semper mi. Nulla consectetur interdum massa, vel porta enim vulputate sed. Maecenas elit quam, egestas eget placerat non, fringilla vel eros. Nam vehicula elementum nulla sed consequat. Phasellus eu erat enim. Praesent at magna non massa dapibus scelerisque in eu lorem.</p>
+		  </section>
+		  <section id="vertblog">
+		      <h2><a href="#vertblog">Folder Three</a></h2>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id lobortis massa. Nunc viverra velit leo, sit amet elementum mi. Fusce posuere nunc a mi tempus malesuada. Curabitur facilisis rhoncus eros eget placerat. Aliquam semper mauris sit amet justo tempor nec lacinia magna molestie. Etiam placerat congue dolor vitae adipiscing. Aliquam ac erat lorem, ut iaculis justo. Etiam mattis dignissim gravida. Aliquam nec justo ante, non semper mi. Nulla consectetur interdum massa, vel porta enim vulputate sed. Maecenas elit quam, egestas eget placerat non, fringilla vel eros. Nam vehicula elementum nulla sed consequat. Phasellus eu erat enim. Praesent at magna non massa dapibus scelerisque in eu lorem.</p>
+		  </section>
+		  <section id="vertportfolio">
+		      <h2><a href="#vertportfolio">Folder Four</a></h2>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id lobortis massa. Nunc viverra velit leo, sit amet elementum mi. Fusce posuere nunc a mi tempus malesuada. Curabitur facilisis rhoncus eros eget placerat. Aliquam semper mauris sit amet justo tempor nec lacinia magna molestie. Etiam placerat congue dolor vitae adipiscing. Aliquam ac erat lorem, ut iaculis justo. Etiam mattis dignissim gravida. Aliquam nec justo ante, non semper mi. Nulla consectetur interdum massa, vel porta enim vulputate sed. Maecenas elit quam, egestas eget placerat non, fringilla vel eros. Nam vehicula elementum nulla sed consequat. Phasellus eu erat enim. Praesent at magna non massa dapibus scelerisque in eu lorem.</p>
+		  </section>
+		  <section id="vertcontact">
+		      <h2><a href="#vertcontact">Folder Five</a></h2>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id lobortis massa. Nunc viverra velit leo, sit amet elementum mi. Fusce posuere nunc a mi tempus malesuada. Curabitur facilisis rhoncus eros eget placerat. Aliquam semper mauris sit amet justo tempor nec lacinia magna molestie. Etiam placerat congue dolor vitae adipiscing. Aliquam ac erat lorem, ut iaculis justo. Etiam mattis dignissim gravida. Aliquam nec justo ante, non semper mi. Nulla consectetur interdum massa, vel porta enim vulputate sed. Maecenas elit quam, egestas eget placerat non, fringilla vel eros. Nam vehicula elementum nulla sed consequat. Phasellus eu erat enim. Praesent at magna non massa dapibus scelerisque in eu lorem.</p>
+		  </section>
+		</div>
+
+
 	</body>
 </html>
